2a0879e8a30672c0abe785b04b3080d863da1256
dzy <632657815@qq.com>
2021-12-19 17:31:06 +0800

    reset 项目重置，后续在此基础上进行迭代学习/开发

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/config/MyBatisConfig.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/config/MyBatisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * MyBatis配置类
  * Created by macro on 2019/4/8.
+ * 用于配置需要动态生成的mapper接口的路径
  */
 @Configuration
 @MapperScan("com.macro.mall.tiny.mbg.mapper")
--- mall-tiny-01/src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
@@ -1,6 +1,5 @@
 package com.macro.mall.tiny.service.impl;
 
-import com.github.pagehelper.PageHelper;
 import com.macro.mall.tiny.mbg.mapper.PmsBrandMapper;
 import com.macro.mall.tiny.mbg.model.PmsBrand;
 import com.macro.mall.tiny.mbg.model.PmsBrandExample;
@@ -10,44 +9,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * PmsBrandService实现类
- * Created by macro on 2019/4/19.
- */
 @Service
 public class PmsBrandServiceImpl implements PmsBrandService {
+
     @Autowired
-    private PmsBrandMapper brandMapper;
+    private PmsBrandMapper pmsBrandMapper;
 
     @Override
     public List<PmsBrand> listAllBrand() {
-        return brandMapper.selectByExample(new PmsBrandExample());
-    }
-
-    @Override
-    public int createBrand(PmsBrand brand) {
-        return brandMapper.insertSelective(brand);
-    }
-
-    @Override
-    public int updateBrand(Long id, PmsBrand brand) {
-        brand.setId(id);
-        return brandMapper.updateByPrimaryKeySelective(brand);
-    }
-
-    @Override
-    public int deleteBrand(Long id) {
-        return brandMapper.deleteByPrimaryKey(id);
-    }
-
-    @Override
-    public List<PmsBrand> listBrand(int pageNum, int pageSize) {
-        PageHelper.startPage(pageNum, pageSize);
-        return brandMapper.selectByExample(new PmsBrandExample());
-    }
-
-    @Override
-    public PmsBrand getBrand(Long id) {
-        return brandMapper.selectByPrimaryKey(id);
+        return pmsBrandMapper.selectByExample(new PmsBrandExample());
     }
 }
